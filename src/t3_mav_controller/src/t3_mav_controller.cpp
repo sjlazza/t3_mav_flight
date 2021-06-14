b2140d97c2ed6510d643c47fb47a365c29851859
Seung Jae Lee <sjlazza@snu.ac.kr>
2021-06-14 18:16:00 +0900

    gimbal now activated

--- src/t3_mav_controller/src/t3_mav_controller.cpp
+++ src/t3_mav_controller/src/t3_mav_controller.cpp
@@ -8,18 +8,17 @@
 #include <mavros_msgs/RCIn.h>
 #include "tf/transform_datatypes.h"
 #include <tf/LinearMath/Quaternion.h>
-// #include <quaternQuaternion.h>
 
 sensor_msgs::JointState rac_servo_value;
 sensor_msgs::Imu tp_att;
 geometry_msgs::Quaternion tp_att_quaternion;
 geometry_msgs::Vector3 tp_att_rpy;
 mavros_msgs::RCIn rc_in;
-trajectory_msgs::JointTrajectory relative_attitude;
+bool servo_sw=false;
 
-void dynamixelstateCallback(const sensor_msgs::JointState& msg){
+void jointstateCallback(const sensor_msgs::JointState& msg){
     rac_servo_value=msg;
-    // ROS_INFO("RR:%lf",rac_servo_value.position[0]);
+    //ROS_INFO("RR:%lf",rac_servo_value.position[0]);
 }
 
 void imu_mavrosCallback(const sensor_msgs::Imu& msg){
@@ -37,43 +36,49 @@ void imu_mavrosCallback(const sensor_msgs::Imu& msg){
 }
 
 void rcin_mavrosCallback(const mavros_msgs::RCIn& msg){
+    // Channel 7 is the servo activation key
     rc_in=msg;
-    // ROS_INFO("Servo_sw:%d", rc_in.channels[7]);
+    if(rc_in.channels[7]>1300){
+	    servo_sw=true;
+    }else{
+	    servo_sw=false;
+    }
+//    ROS_INFO("Servo_sw:%d", rc_in.channels[7]);
 }
 
-void relative_attitude_package(float relative_roll, float relative_pitch){
-    std::vector<trajectory_msgs::JointTrajectoryPoint> points_n(1);
-    points_n[0].positions.resize(2);
-    // points_n[1].positions.resize(1);
-    points_n[0].positions[0]=relative_roll;
-    points_n[0].positions[1]=relative_pitch-M_PI/2;
+sensor_msgs::JointState servo_msg_create(double rr, double rp){
+	sensor_msgs::JointState servo_msg;
 
-    // relative_attitude.header.stamp  = ros::Time::now();
-    relative_attitude.joint_names.resize(2);
-    relative_attitude.points.resize(1);
+	servo_msg.header.stamp=ros::Time::now();
 
-    relative_attitude.joint_names[0]= "relative_pitch";
-    relative_attitude.points[0]     = points_n[0];
-    relative_attitude.joint_names[1]= "relative_roll";
-    // relative_attitude.points[1]     = points_n[1];
+	servo_msg.name.resize(2);
+	servo_msg.name[0]="id_1";
+	servo_msg.name[1]="id_2";
+
+	servo_msg.position.resize(2);
+	servo_msg.position[0]=rr-M_PI/2;
+	servo_msg.position[1]=rp;
+
+	return servo_msg;
 }
 
 int main(int argc, char **argv){
     ros::init(argc, argv,"t3_mav_controller");
     ros::NodeHandle nh;
 
-    ros::Publisher dynamixel_traj_   = nh.advertise<trajectory_msgs::JointTrajectory>("/dynamixel_workbench/joint_trajectory",1000);
+    ros::Publisher goal_dynamixel_position_  = nh.advertise<sensor_msgs::JointState>("goal_dynamixel_position",1000);
 
-    ros::Subscriber dynamixel_state = nh.subscribe("dynamixel_workbench/joint_states",1000,dynamixelstateCallback);
+    ros::Subscriber dynamixel_state = nh.subscribe("joint_states",1000,jointstateCallback);
     ros::Subscriber mavros_att      = nh.subscribe("mavros/imu/data",1000,imu_mavrosCallback);
     ros::Subscriber rc_in           = nh.subscribe("mavros/rc/in",1000,rcin_mavrosCallback);
-    ros::Rate loop_rate(1);
+    ros::Rate loop_rate(100);
     
     while(ros::ok()){
-        
-//        relative_attitude_package(0,0);
-//        dynamixel_traj_.publish(relative_attitude);
-
+     	if(servo_sw==true){
+		goal_dynamixel_position_.publish(servo_msg_create(-tp_att_rpy.x,-tp_att_rpy.y));
+	}else{
+		goal_dynamixel_position_.publish(servo_msg_create(0,0)); 
+	}
         ros::spinOnce();
         loop_rate.sleep();
 
